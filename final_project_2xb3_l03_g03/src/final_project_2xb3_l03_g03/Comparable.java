f79ba577cad1ca189c457da322aa72ee13af6a61
Senni Tan <tans28@mcmaster.ca>
2020-03-14 01:30:09 -0400

    updated sorting

--- final_project_2xb3_l03_g03/src/final_project_2xb3_l03_g03/Comparable.java
+++ final_project_2xb3_l03_g03/src/final_project_2xb3_l03_g03/Comparable.java
@@ -1,5 +1,8 @@
 package final_project_2xb3_l03_g03;
 
 public class Comparable {
-
+	public static int Compare(Comparable a, Comparable b) {
+		//implement here
+		return 0;
+	}
 }
--- final_project_2xb3_l03_g03/src/final_project_2xb3_l03_g03/Sorting.java
+++ final_project_2xb3_l03_g03/src/final_project_2xb3_l03_g03/Sorting.java
@@ -1,5 +1,36 @@
 package final_project_2xb3_l03_g03;
 
-public class Sorting extends Comparable{
+public class Sorting extends Comparable {
 	
+	private static void exch(Comparable[] a, int i, int j) {
+		Comparable t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+	
+	public static void sort(Comparable[] a) {
+		StdRandom.shuffle(a);
+		sort(a, 0, a.length - 1);
+	}
+	
+	private static void sort(Comparable[] a, int lo, int hi) {
+		if (hi <= lo) return;
+		int j = partition(a, lo, hi);
+		sort(a, lo, j - 1);
+		sort(a, j + 1, hi);
+	}
+	
+	private static int partition(Comparable[] a, int lo, int hi) {
+		int i = lo;
+		int j = hi + 1;
+		Comparable v = a[lo];
+		while(true) {
+			while (Compare(a[++i], v) < 0) if (i == hi) break;
+			while (Compare(v, a[--j]) < 0) if (j == lo) break;
+			if (i >= j) break;
+			exch(a, i, j);
+		}
+		exch(a, lo, j);
+		return j;
+	}
 }
