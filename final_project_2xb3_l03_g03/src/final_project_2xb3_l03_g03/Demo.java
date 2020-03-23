c1c20f7302bf99e7990c685866729c902627d0f6
duz12 <duz12@mcmaster.ca>
2020-03-23 15:06:24 -0400

    update

---
+++ final_project_2xb3_l03_g03/src/final_project_2xb3_l03_g03/Demo.java
@@ -0,0 +1,16 @@
+public class Demo{
+    public static void main(String[] args){
+        //read from the csv files, get an arraylist of jobs
+        //also get an arraylist of regions
+        //////////////////////////////////////////////////////
+        //sort the arraylist of region, and the regions alphabetically
+        //////////////////////////////////////////////////////
+        //show the options of catagories, regions
+        //get the user information
+        //select the given region and catagory, and have a newarraylist
+        /////////////////////////////////////////////////////
+        //create a graph according to that
+        //show the list of jobs(sorted by outlook)
+        //allow the user have a close look at one job(outlook, relevant jobs)
+    }
+}
