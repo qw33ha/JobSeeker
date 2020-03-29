4780c08d25e0bbd30e3980d716f233ef850cfff3
wanggengyun <wanggengyun@wanggengyundeMacBook-Pro.local>
2020-03-28 22:20:23 -0400

    update Searching.java after Thursday's discussion

--- final_project_2xb3_l03_g03/src/final_project_2xb3_l03_g03/Searching.java
+++ final_project_2xb3_l03_g03/src/final_project_2xb3_l03_g03/Searching.java
@@ -4,57 +4,114 @@ import java.util.ArrayList;
 
 public class Searching {
 
-    public static ArrayList<Job> field_search(ArrayList<Job> dataset, int field){
-		ArrayList<Job> new_dataset = new ArrayList<Job>();
-		for(int i = 0; i < dataset.size(); i++) {
-			Job currentjob = dataset.get(i);
-			if(currentjob.get_noc(0) == field)
-				new_dataset.add(currentjob);
-		}
-		return new_dataset;
-	}
+    public static int Location_Search(Job[] jobs , String location){
 
-    public static ArrayList<Job> region_search(ArrayList<Job> dataset, int region){
-		ArrayList<Job> new_dataset = new ArrayList<Job>();
-		for(int i = 0; i < dataset.size(); i++) {
-			Job currentjob = dataset.get(i);
-			if(currentjob.get_region() == region)
-				new_dataset.add(currentjob);
+        int left = 0;
+        int right = jobs.length - 1;
+
+        while(left <= right ) {
+            int mid = (left + (right - left) / 2);
+            int res = location.compareTo(jobs[mid].get_location());
+            if( res == 0 ) {
+                while(mid >= 0) {
+                    if(0!=location.compareTo(jobs[mid].get_location())) {
+                        break;
+                    }
+                    mid--;
+                }
+                if(mid <= -1 ) {
+                    return 0;
+                }
+                return mid + 1;//plus 1 because the while loop did 1 more subtraction.
+            }else if( res < 0 ) {
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+	}
+	
+	public static ArrayList<Job> LocationSearch(
+			ArrayList<Job> jobs , String location){
+		ArrayList<Job> new_jobs = new ArrayList<Job>();
+		Job[] temp = jobs.toArray(new Job[jobs.size()]);
+		int index = Location_Search(temp, location);
+		if(index == -1) {
+			System.out.println("no job found for given location");
+			return new_jobs;
 		}
-		if (new_dataset.size() == 0) {
-			System.out.println("no job found for the given region");
-			return null;
-		}
-		else {
-			for (int i = 0; i < new_dataset.size(); i++) {
-				new_dataset.get(i).printInfo();
+		for (; index < jobs.size(); index++) {
+			if (temp[index].get_location() != location) {
+				return new_jobs;
+			}
+			else {
+				new_jobs.add(temp[index]);
+				temp[index].printInfo();
 				System.out.println();
 			}
-		}	
-		return new_dataset;
+		}
+		return new_jobs;
 	}
+	
+	public static int noc_Search(Job[] jobs , int noc){
+		int left = 0;
+        int right = jobs.length - 1;
 
-    public static ArrayList<Job> noc_search(ArrayList<Job> dataset, int[] noc){
-		ArrayList<Job> new_dataset = new ArrayList<Job>();
-		for(int i = 0; i < dataset.size(); i++) {
-			Job currentjob = dataset.get(i);
-			if(currentjob.get_noc(0) == noc[0] & 
-					currentjob.get_noc(1) == noc[1]& 
-					currentjob.get_noc(2) == noc[2]& 
-					currentjob.get_noc(3) == noc[3])
-				new_dataset.add(currentjob);
+        while(left <= right ) {
+            int mid = (left + (right - left) / 2);
+            int res;
+            if (noc == jobs[mid].get_noc(0)) {
+            	res = 0;
+            }
+            else if (noc > jobs[mid].get_noc(0)) {
+            	res = 1;
+            }
+            else {
+            	res = -1;
+            }
+            if( res == 0 ) {
+                while(mid >= 0) {
+                    if(noc != jobs[mid].get_noc(0)) {
+                        break;
+                    }
+                    mid--;
+                }
+                if(mid <= -1 ) {
+                    return 0;
+                }
+                return mid + 1;//plus 1 because the while loop did 1 more subtraction.
+            }else if( res < 0 ) {
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+	}
+	
+	public static ArrayList<Job> NocSearch(
+			ArrayList<Job> jobs , int noc){
+		ArrayList<Job> new_jobs = new ArrayList<Job>();
+		Job[] temp = jobs.toArray(new Job[jobs.size()]);
+		int index = noc_Search(temp, noc);
+		if(index == -1) {
+			System.out.println("no job found for given noc");
+			return new_jobs;
 		}
-		if (new_dataset.size() == 0) {
-			System.out.println("no job found for the given location");
-			return null;
-		}
-		else {
-			for (int i = 0; i < new_dataset.size(); i++) {
-				new_dataset.get(i).printInfo();
+		for (; index < jobs.size(); index++) {
+			if (temp[index].get_noc(0) != noc) {
+				return new_jobs;
+			}
+			else {
+				new_jobs.add(temp[index]);
+				temp[index].printInfo();
 				System.out.println();
 			}
-		}	
-		return new_dataset;
+		}
+		return new_jobs;
 	}
 
 }
