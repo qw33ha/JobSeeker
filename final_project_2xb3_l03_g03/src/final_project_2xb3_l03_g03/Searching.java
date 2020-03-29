7a00528a675da5ac8f02d8a743067d9e1fd2cf23
wanggengyun <wanggengyun@wanggengyundeMacBook-Pro.local>
2020-03-29 00:48:10 -0400

    improve the Searching algorithm

--- final_project_2xb3_l03_g03/src/final_project_2xb3_l03_g03/Searching.java
+++ final_project_2xb3_l03_g03/src/final_project_2xb3_l03_g03/Searching.java
@@ -6,110 +6,140 @@ public class Searching {
 
     public static int Location_Search(Job[] jobs , String location){
 
-        int left = 0;
-        int right = jobs.length - 1;
-
-        while(left <= right ) {
-            int mid = (left + (right - left) / 2);
-            int res = location.compareTo(jobs[mid].get_location());
-            if( res == 0 ) {
-                while(mid >= 0) {
-                    if(0!=location.compareTo(jobs[mid].get_location())) {
-                        break;
-                    }
-                    mid--;
-                }
-                if(mid <= -1 ) {
-                    return 0;
-                }
-                return mid + 1;//plus 1 because the while loop did 1 more subtraction.
-            }else if( res < 0 ) {
-                right = mid - 1;
-            }else {
-                left = mid + 1;
-            }
-        }
-
-        return -1;
+		int l = 0, r = jobs.length - 1; 
+        while (l <= r) { 
+            int m = l + (r - l) / 2; 
+  
+            int res = location.compareTo(jobs[m].get_location()); 
+  
+            // Check if x is present at mid 
+            if (res == 0) 
+                return m; 
+  
+            // If x greater, ignore left half 
+            if (res > 0) 
+                l = m + 1; 
+  
+            // If x is smaller, ignore right half 
+            else
+                r = m - 1; 
+        } 
+  
+        return -1; 
+	}
+	
+	public static int[] Location_Range(Job[] jobs , String location, int mid){
+		int low = mid;
+		int hi = mid;
+		while(hi < jobs.length) {
+			if(0!=location.compareTo(jobs[hi].get_location())) {
+                break;
+			}
+			hi++;
+		}
+		hi--;
+		while(low >= 0) {
+			if(0!=location.compareTo(jobs[low].get_location())) {
+                break;
+			}
+			low--;
+		}
+		low++;
+		int[] a = new int[] {low, hi};
+		return a;
 	}
 	
 	public static ArrayList<Job> LocationSearch(
 			ArrayList<Job> jobs , String location){
 		ArrayList<Job> new_jobs = new ArrayList<Job>();
 		Job[] temp = jobs.toArray(new Job[jobs.size()]);
-		int index = Location_Search(temp, location);
-		if(index == -1) {
+		int mid = Location_Search(temp, location);
+		if(mid == -1) {
 			System.out.println("no job found for given location");
 			return new_jobs;
 		}
-		for (; index < jobs.size(); index++) {
-			if (temp[index].get_location() != location) {
-				return new_jobs;
-			}
-			else {
-				new_jobs.add(temp[index]);
-				temp[index].printInfo();
-				System.out.println();
-			}
+		int [] a = Location_Range(temp, location, mid);
+		int low = a[0];
+		int hi = a[1];
+		while (low <= hi) {
+			temp[low].printInfo();
+			new_jobs.add(temp[low]);
+			low++;
 		}
 		return new_jobs;
 	}
 	
 	public static int noc_Search(Job[] jobs , int noc){
-		int left = 0;
-        int right = jobs.length - 1;
 
-        while(left <= right ) {
-            int mid = (left + (right - left) / 2);
+		int l = 0, r = jobs.length - 1; 
+        while (l <= r) { 
+            int m = l + (r - l) / 2; 
+  
             int res;
-            if (noc == jobs[mid].get_noc(0)) {
+            
+            if (noc == jobs[m].get_noc(0)) {
             	res = 0;
             }
-            else if (noc > jobs[mid].get_noc(0)) {
+            else if (noc > jobs[m].get_noc(0)) {
             	res = 1;
             }
             else {
             	res = -1;
             }
-            if( res == 0 ) {
-                while(mid >= 0) {
-                    if(noc != jobs[mid].get_noc(0)) {
-                        break;
-                    }
-                    mid--;
-                }
-                if(mid <= -1 ) {
-                    return 0;
-                }
-                return mid + 1;//plus 1 because the while loop did 1 more subtraction.
-            }else if( res < 0 ) {
-                right = mid - 1;
-            }else {
-                left = mid + 1;
-            }
-        }
-
-        return -1;
+  
+            // Check if x is present at mid 
+            if (res == 0) 
+                return m; 
+  
+            // If x greater, ignore left half 
+            if (res > 0) 
+                l = m + 1; 
+  
+            // If x is smaller, ignore right half 
+            else
+                r = m - 1; 
+        } 
+  
+        return -1; 
+	}
+	
+	public static int[] noc_Range(Job[] jobs , int noc, int mid){
+		int low = mid;
+		int hi = mid;
+		while(hi < jobs.length) {
+			if(noc != jobs[hi].get_noc(0)) {
+                break;
+			}
+			hi++;
+		}
+		hi--;
+		while(low >= 0) {
+			if(noc != jobs[low].get_noc(0)) {
+                break;
+			}
+			low--;
+		}
+		low++;
+		int[] a = new int[] {low, hi};
+		return a;
 	}
 	
 	public static ArrayList<Job> NocSearch(
 			ArrayList<Job> jobs , int noc){
 		ArrayList<Job> new_jobs = new ArrayList<Job>();
 		Job[] temp = jobs.toArray(new Job[jobs.size()]);
-		int index = noc_Search(temp, noc);
-		if(index == -1) {
+		int mid = noc_Search(temp, noc);
+		if(mid == -1) {
 			System.out.println("no job found for given noc");
 			return new_jobs;
 		}
-		for (; index < jobs.size(); index++) {
-			if (temp[index].get_noc(0) != noc) {
-				return new_jobs;
-			}
-			else {
-				new_jobs.add(temp[index]);
-				temp[index].printInfo();
-				System.out.println();
-			}
+		int [] a = noc_Range(temp, noc, mid);
+		int low = a[0];
+		int hi = a[1];
+		while (low <= hi) {
+			temp[low].printInfo();
+			new_jobs.add(temp[low]);
+			low++;
 		}
 		return new_jobs;
 	}
