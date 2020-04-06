2e78bfb682c5bd56698c6bf8db7f207d114e5cff
Senni Tan <tans28@mcmaster.ca>
2020-04-06 17:46:40 -0400

    unit tests

---
+++ final_project_2xb3_l03_g03/src/final_project_2xb3_l03_g03/ComparableTest.java
@@ -0,0 +1,39 @@
+package final_project_2xb3_l03_g03;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ComparableTest extends Comparable{
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCompareString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareOutlook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareNOC() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareRegionS() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ final_project_2xb3_l03_g03/src/final_project_2xb3_l03_g03/SortingTest.java
@@ -0,0 +1,59 @@
+package final_project_2xb3_l03_g03;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SortingTest extends Sorting {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSortString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSortOutlook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSortNOC() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSortRegionS() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareOutlook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareNOC() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareRegionS() {
+		fail("Not yet implemented");
+	}
+
+}
