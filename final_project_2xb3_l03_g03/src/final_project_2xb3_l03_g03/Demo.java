86fd9d7b41bc957ebbbb03a6593966316015d20a
duz12 <duz12@mcmaster.ca>
2020-04-08 12:37:53 -0400

    modularity

--- final_project_2xb3_l03_g03/src/final_project_2xb3_l03_g03/Demo.java
+++ final_project_2xb3_l03_g03/src/final_project_2xb3_l03_g03/Demo.java
@@ -1,14 +1,27 @@
-package final_project_2xb3_l03_g03;
 import java.io.*;
-import java.security.InvalidParameterException;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
-
 public class Demo extends Sorting{
+	/**
+	 * @brief Connect related jobs in the graph
+	 * @param G The graph of jobs
+	 * @param jobs ArrayList of jobs
+	 */
+	public static void connectjobs(Graph G, ArrayList<Job> jobs) {
+		for(int i = 0; i < jobs.size(); i++) {
+			for(int j = i + 1; j < jobs.size(); j++) {
+				if(jobs.get(i).get_noc(1) == jobs.get(j).get_noc(1) &&
+					jobs.get(i).get_noc(2) == jobs.get(j).get_noc(2))
+					G.addedge(i, j);
+			}
+		}
+	}
+	/**
+	 * @brief Client module
+	 * @exception FileNotFoundException, IOException, InterruptedException When the data base misses, input errors
+	 */
 	public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException{
-		PrintWriter output = new PrintWriter("output.txt");//output init
-
 		ArrayList<Job> joblist = new ArrayList<Job>();
 		DataProcess data = new DataProcess();
 		//joblist is an arraylist of all jobs in dataset
@@ -32,18 +45,13 @@ public class Demo extends Sorting{
 		int usercategory = -1;
 		while(true) {
 			System.out.println("Please choose from these regions and enter the four digit code of the region you are interested in:");
-			
-
 			for(int i = 0; i < SIZE - 1; i++) {
 				if(!sortedjoblist.get(i).get_regions().equals(sortedjoblist.get(i+1).get_regions()) && sortedjoblist.get(i).get_region() != sortedjoblist.get(i+1).get_region()) {
 					System.out.println(sortedjoblist.get(i).get_regions() + "    : " + sortedjoblist.get(i).get_region());					
-					output.println(sortedjoblist.get(i).get_regions() + "    : " + sortedjoblist.get(i).get_region());					
 				}
 			}
-			if(sortedjoblist.get(SIZE-1).get_region() != sortedjoblist.get(SIZE-2).get_region()) {
+			if(sortedjoblist.get(SIZE-1).get_region() != sortedjoblist.get(SIZE-2).get_region())
 				System.out.println(sortedjoblist.get(SIZE-1).get_regions() + "    : " + sortedjoblist.get(SIZE-1).get_region());
-				output.println(sortedjoblist.get(SIZE-1).get_regions() + "    : " + sortedjoblist.get(SIZE-1).get_region());
-				}
 			//now take user input
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			String input = reader.readLine();
@@ -58,14 +66,10 @@ public class Demo extends Sorting{
 			System.out.println("Please enter the correct code");
 			TimeUnit.SECONDS.sleep(1);
 		}
-		output.println("User choosed region " + userlocation);//txt
         //////////////////////////////////////////////////////
 		//now let the user choose category
-		for(int i = 0; i < NOC.Noc.length; i++) {
+		for(int i = 0; i < NOC.Noc.length; i++)
 			System.out.println(i + ": " + NOC.Noc[i]);
-			output.println(i + ": " + NOC.Noc[i]);
-
-			}
 		while(true) {
 			System.out.println("\nPlease enter the index of the categories you are interested here: ");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -80,8 +84,6 @@ public class Demo extends Sorting{
 			catch(NumberFormatException e) {}
 			System.out.println("Please enter the correct index");
 		}
-		output.println("User choosed category " + usercategory);//txt
-
         //select the given region and category, and have a new arraylist
 		ArrayList<Job> jobinthisregion = Searching.LocationSearch(sortedjoblist,code2string[userlocation]);
 		temp = jobinthisregion.toArray(new Job[jobinthisregion.size()]);
@@ -93,24 +95,14 @@ public class Demo extends Sorting{
 		/////////////////////////////////////////////////////
         //create a graph according to that
 		Graph G = new Graph(specificjobs.size());
-		for(int i = 0; i < specificjobs.size(); i++) {
-			for(int j = i + 1; j < specificjobs.size(); j++) {
-				if(specificjobs.get(i).get_noc(1) == specificjobs.get(j).get_noc(1) &&
-					specificjobs.get(i).get_noc(2) == specificjobs.get(j).get_noc(2))
-					G.addedge(i, j);
-			}
-		}
+		connectjobs(G,specificjobs);
         //show the list of jobs(sorted by outlook)
 		System.out.println("Searching for jobs...");
-		output.println("Jobs found: ");//txt
-
 		int count = 0;
 		outer:
 		while(count < specificjobs.size() - 8) {
 			for(int i = 0; i < 8; i++) {
 				specificjobs.get(count).printbriefInfo();
-				output.println(specificjobs.get(count).getbriefInfo());//txt
-
 				count++;
 			}
 			System.out.println("\n\nPress Enter to see more, type 'quit' to quit scaning and have a close look a one job");
@@ -127,29 +119,19 @@ public class Demo extends Sorting{
         //allow the user have a close look at one job(outlook, relevant jobs)
 		while(true) {
 			System.out.println("\n\n\nPlease type the Noc code of the job you are interested in:");
-			output.println("Quit scanning");//txt
-
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			String noccode = reader.readLine();
-			output.println("Input noc " + noccode);//txt
-
 			int tem;
 			try {
 				int code = Integer.parseInt(noccode);
 				for(tem = 0; tem < specificjobs.size(); tem++) {
 					if(code == specificjobs.get(tem).getnoc()) {
 						specificjobs.get(tem).printInfo();
-						output.println(specificjobs.get(tem).getInfo());//txt
-
 						DFS dfs = new DFS(G,tem);
 						System.out.println("Number of relatedjobs: " + dfs.count());
-						output.println("Number of relatedjobs: " + dfs.count());//txt
-
 						for(int k = 0; k < specificjobs.size(); k++)
 							if(dfs.hasPathTo(k)) {
 								specificjobs.get(k).printbriefInfo();
-								output.println(specificjobs.get(k).getbriefInfo());//txt
-
 							}
 					}
 				}
@@ -157,6 +139,5 @@ public class Demo extends Sorting{
 			}
 			catch(NumberFormatException e) {}
 		}
-		output.close();
-	}
+    }
 }
