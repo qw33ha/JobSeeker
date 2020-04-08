a63d416603f30f39fc344e39a1b8e07fa6072f0e
Wenzhi Wang <wangw95@mcmaster.ca>
2020-04-07 22:24:32 -0400

    Update DataProcess.java

--- final_project_2xb3_l03_g03/src/final_project_2xb3_l03_g03/DataProcess.java
+++ final_project_2xb3_l03_g03/src/final_project_2xb3_l03_g03/DataProcess.java
@@ -2,7 +2,7 @@
   *  Author: Wang wenzhi
   *  Revised: April 7, 2020
   *  
-  *  Description: A module that read in and process the dataset
+  *  Description: A module that reads in and processes the dataset
  */
 package final_project_2xb3_l03_g03;
 
